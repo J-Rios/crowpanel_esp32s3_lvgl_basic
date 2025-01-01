6147a3b8e10d861c7e8554b3ff45b39bf7db8802
J-Rios <jrios.github@gmail.com>
2025-01-01 11:28:21 +0100

    [IDF] Base PIO Project

---
+++ projects/espidf/espidf_base/src/config/config.h
@@ -0,0 +1,132 @@
+/**
+ * @file    config.h
+ * @author  Jose Miguel Rios Rubio <jrios.github@gmail.com>
+ * @date    2024-12-27
+ * @version 1.0.0
+ *
+ * @section DESCRIPTION
+ *
+ * Configuration Data header file.
+ *
+ * @section LICENSE
+ *
+ * MIT License
+ *
+ * Copyright (c) 2024 Jose Miguel Rios Rubio
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+/*****************************************************************************/
+
+/* Include Guard */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/*****************************************************************************/
+
+/* Libraries */
+
+// Standard C++ Libraries
+#include <cstdint>
+
+/*****************************************************************************/
+
+/* Configurations */
+
+// Default Project Name
+#if !defined(SET_PROJECT_NAME)
+    #define SET_PROJECT_NAME "esp32_project"
+#endif
+
+// Default Firmware Application Version if not provided
+#if !defined(SET_FW_APP_VERSION_X)
+    #define SET_FW_APP_VERSION_X 1
+#endif
+#if !defined(SET_FW_APP_VERSION_Y)
+    #define SET_FW_APP_VERSION_Y 0
+#endif
+#if !defined(SET_FW_APP_VERSION_Z)
+    #define SET_FW_APP_VERSION_Z 0
+#endif
+
+// I2C Pins Touchscreen
+#define SDA_FT6236 38
+#define SCL_FT6236 39
+
+// GPIO Pins
+#define IO_LCD_BACKLIGHT 46
+#define IO_BUZZER 20
+
+/*****************************************************************************/
+
+/* System Configuration Constants */
+
+namespace ns_const
+{
+    /**
+     * @brief Default Project Name.
+     */
+    static constexpr char PROJECT_NAME[] = SET_PROJECT_NAME;
+
+    /**
+     * @brief Firmware Application Major Version.
+     */
+    static constexpr uint8_t FW_APP_VERSION_X
+        = static_cast<uint8_t>(SET_FW_APP_VERSION_X);
+
+    /**
+     * @brief Firmware Application Minor Version.
+     */
+    static constexpr uint8_t FW_APP_VERSION_Y
+        = static_cast<uint8_t>(SET_FW_APP_VERSION_Y);
+
+    /**
+     * @brief Firmware Application Patch Version.
+     */
+    static constexpr uint8_t FW_APP_VERSION_Z
+        = static_cast<uint8_t>(SET_FW_APP_VERSION_Z);
+
+    /**
+     * @brief Default Debug Serial Port Speed.
+     */
+    static constexpr uint32_t DEFAULT_UART_BAUD_RATE = 115200U;
+
+    /**
+     * @brief Buzzer Minimum Frequency.
+     */
+    static constexpr const uint16_t BUZZER_MIN_FREQ_HZ = 100U;
+
+    /**
+     * @brief Buzzer Maximum Frequency.
+     */
+    static constexpr const uint16_t BUZZER_MAX_FREQ_HZ = 2700U;
+
+    /**
+     * @brief Buzzer Duty Cycle.
+     */
+    static constexpr const uint8_t BUZZER_DUTY_CYCLE = 50U;
+}
+
+/*****************************************************************************/
+
+/* Include Guard Close */
+
+#endif /* CONFIG_H */
---
+++ projects/espidf/espidf_base/src/main.cpp
@@ -0,0 +1,95 @@
+/**
+ * @file    main.cpp
+ * @author  Jose Miguel Rios Rubio <jrios.github@gmail.com>
+ * @date    2024-12-30
+ * @version 1.0.0
+ *
+ * @section DESCRIPTION
+ *
+ * Project Main file.
+ *
+ * @section LICENSE
+ *
+ * MIT License
+ *
+ * Copyright (c) 2024 Jose Miguel Rios Rubio
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+/*****************************************************************************/
+
+/* Libraries */
+
+// Standard C++ Libararies
+#include <stdio.h>
+
+// FreeRTOS Libraries
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+// ESP-IDF Framework
+#include "esp_log.h"
+#include "esp_system.h"
+
+// Project Headers
+#include "config/config.h"
+
+/*****************************************************************************/
+
+/* Function Prototypes */
+
+extern "C" { void app_main(void); }
+
+uint32_t psram_get_size();
+
+/*****************************************************************************/
+
+/* Main Function */
+
+void app_main(void)
+{
+    using namespace ns_const;
+
+    printf("\n\n---------\n\n");
+    printf("Project: %s\n", PROJECT_NAME);
+    printf("FW Version: v%u.%u.%u (%s %s)\n", FW_APP_VERSION_X,
+        FW_APP_VERSION_Y, FW_APP_VERSION_Z, __DATE__, __TIME__);
+    printf("ESP-IDF Version: %s\n", esp_get_idf_version());
+    printf("\n");
+
+    while(1)
+    {
+        static uint32_t uptime = 0U;
+
+        printf("Uptime: %lu seconds\n", uptime);
+        uptime = uptime+ 1U;
+
+        vTaskDelay(1000U/portTICK_PERIOD_MS);
+    }
+}
+
+/*****************************************************************************/
+
+/* Auxiliary Functions */
+
+// None
+
+/*****************************************************************************/
